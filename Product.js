500851849b90f1c2efb8bed5924c5ac96c16aed8
OlgaPast <34867785+Olga-Past@users.noreply.github.com>
2018-01-03 16:57:48 +0200

    Update Product.js

--- Product.js
+++ Product.js
@@ -8,80 +8,76 @@ var prodcuts = [{
 		inventory : 80
 	}];
 	
-
 	class ProductLineItem {
 		constructor(product) {
 			this.name = product.name;
 			this.quantity = 1;
 			this.price = product.price * this.quantity;
 			this.inventory = product.inventory;
-		}	
+		}
+
 		getName() {
         return this.name;
     }
 
-    getPrice() {
+   		getPrice() {
         return this.price;
     }
 
-    getQuantity() {
+    	getQuantity() {
         return this.quantity;
     }
 
     setQuantity(quantity) {
         this.quantity = quantity;
-   
+        this.price = product.price * this.quantity;
     }						 
-			}
-	
-	
-	var basket = (function(){
-		let productBasket = [];
+		}
+		
+			var basket = (function(){
+			let productBasket = [];
+
+			getItemCount: function() {
+           	return productBasket.length;
+    },
 
 		return {
-			addProduct : function(productID){
-				var index = products.findIndex(p => p.name == productID.name);
-				if(index == null) {
-					console.log("Товар уже в кошику");
-				return;
+		addProduct : function(productID){
+			var index = products.findIndex(p => p.name == productID.name);
+			if(index == null) {
+				console.log("Товар уже в кошику");
+			return;
 				}
-        productBasket.push(productID);
-
+       		 productBasket.push(productID);
     },
     
+		removeProduct : function(productID){
+		   	 var index = products.findIndex(p => p.name == productID.name);
+		   	 products.splice(index, 1);
+	},
 
-			removeProduct : function(productID){
-		    var index = products.findIndex(p => p.name == productID.name);
-		    products.splice(index, 1);
- 
-},
-
-			updateProductQuantity : function(productID, quantity) {
+		updateProductQuantity : function(productID, quantity) {
 			var index = products.findIndex(p => p.name == productID.name);
 			if(index == undefined) {
 				console.log("У кошик нічого не внеслось");
-				return;
+			return;
 			}         
 			if (productID.inventory == 0)
 				console.log("Кошик порожній");
-				return;	
+			return;	
 			productID.inventory -= quantity;
 			p.quantity += quantity;
-		},
+	},
 
-		getItemCount: function() {
-            return productBasket.length;
-        },
-
-			getTotalPrice : function(){
-				var q = this.getItemCount(),price=0;
+		getTotalPrice : function(){
+			var q = this.getItemCount(),price=0;
             while(q--){
                 price+= productBasket[q].price; 
             }
             return price;
-  }
+  	}
 
-  			}
+  		}
 		
 	})();
 
